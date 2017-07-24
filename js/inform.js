2d830fcd37832d47ec0365b5687f7fc2cdddaf31
unknown <1733025299@qq.com>
2017-07-24 10:31:13 +0800

    modify the article page

--- js/inform.js
+++ js/inform.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    // 新闻列表
+    var page = 1;
+    var pageNumber = 0;
+
     $(function getArticle() {
         $.ajax({
             type: 'post',
@@ -20,11 +24,7 @@ $(document).ready(function () {
                         pageNum = pageNum + 1;
                     }
 
-                    for (var n = 0; n < pageNum; n++) {
-                        $('.page .pagination').append('<li><a href="#">' + count + '</a></li>');
-                        count++;
-                    }
-                    $('.page ul').append('<li class="' + count + '"><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>');
+                    pageNumber = pageNum;
 
                     for (var i = 0; i < articleLen; i++) {
                         var article = list[i].artiTitle.artiData.replace(re1, '');
@@ -43,19 +43,95 @@ $(document).ready(function () {
         });
     });
 
-    $()
+    $('.next').click(function () {
+        $(function getArticle(page) {
+            page++;
 
-// test
-    var string = ['大家哈迪噢扫ID梵高还得分工好大夫', '杰克金多或若过或错', 'q', '2', 'r', 'sdfsdf'];
-    var num = parseInt(string.length / 2);
-    var num2 = string.length % 2;
-    alert(num2);
+            if (page > pageNum) {
+                alert('已经到文章最后一页~');
+                return true;
+            }
 
+            $.ajax({
+                type: 'post',
+                url: ' /ShowBriefArticle',
+                dataType: 'json',
+                data: {
+                    pageNum: page
+                },
+                success: function (data) {
+                    if (data.status === 200) {
+                        var list = data.jsonStrArray.artiList;
+                        var articleLen = list.length;
+                        var re1 = new RegExp("<.+?>", "g");
 
-    var count = 1;
-    for (var n = 0; n < 4; n++) {
-        $('.page ul').append('<li><a href="#">' + count + '</a></li>')
-        count++;
-    }
-    $('.page ul').append('<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>')
+                        var article = list[0].artiTitle.artiData.replace(re1, '');
+                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3>' +
+                            list[0].artiTitle + '</h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
+                        $('.main .content').html(inform);
+
+                        for (var i = 1; i < articleLen; i++) {
+                            var article = list[i].artiTitle.artiData.replace(re1, '');
+                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3>' +
+                                list[i].artiTitle + '</h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
+                            $('.main .content').append(inform);
+                        }
+                    }
+                    else {
+                        alert(data.errMsg + data.status);
+                    }
+                },
+                error: function (jqXHR) {
+                    alert('未知错误：' + jqXHR.status);
+                }
+            });
+        });
+    });
+
+    $('.previous').click(function () {
+        $(function getArticle(page) {
+            page--;
+
+            if (page === 0) {
+                alert('已经到文章第一页~');
+                return true;
+            }
+
+            $.ajax({
+                type: 'post',
+                url: ' /ShowBriefArticle',
+                dataType: 'json',
+                data: {
+                    pageNum: page
+                },
+                success: function (data) {
+                    if (data.status === 200) {
+                        var list = data.jsonStrArray.artiList;
+                        var articleLen = list.length;
+                        var re1 = new RegExp("<.+?>", "g");
+
+                        var article = list[0].artiTitle.artiData.replace(re1, '');
+                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3>' +
+                            list[0].artiTitle + '</h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
+                        $('.main .content').html(inform);
+
+                        for (var i = 1; i < articleLen; i++) {
+                            var article = list[i].artiTitle.artiData.replace(re1, '');
+                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3>' +
+                                list[i].artiTitle + '</h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
+                            $('.main .content').append(inform);
+                        }
+                    }
+                    else {
+                        alert(data.errMsg + data.status);
+                    }
+                },
+                error: function (jqXHR) {
+                    alert('未知错误：' + jqXHR.status);
+                }
+            });
+        });
+    });
+
+    // 新闻详情
 });
