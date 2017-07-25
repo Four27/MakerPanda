acbb328359124691900d3b429a3f9615139e414b
Shirley <1733025299@qq.com>
2017-07-25 17:04:36 +0800

    modified inform

--- js/inform.js
+++ js/inform.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
                     var re1 = new RegExp("<.+?>", "g");
                     var pageNum = parseInt(data.jsonStrArray.artiNum / 15);
                     var pagemode = data.jsonStrArray.artiNum % 15;
-                    var count = 1;
+                    var count = 0;
 
                     if (pagemode !== 0) {
                         pageNum = pageNum + 1;
@@ -28,9 +28,10 @@ $(document).ready(function () {
 
                     for (var i = 0; i < articleLen; i++) {
                         var article = list[i].artiTitle.artiData.replace(re1, '');
-                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a id="' + list[i].artiId + '">'
-                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>'
+                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="1" id="' + count + '">'
+                        list[i].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>'
                         $('.main .content').append(inform);
+                        count++;
                     }
                 }
                 else {
@@ -64,17 +65,20 @@ $(document).ready(function () {
                         var list = data.jsonStrArray.artiList;
                         var articleLen = list.length;
                         var re1 = new RegExp("<.+?>", "g");
+                        var count = 0
 
                         var article = list[0].artiTitle.artiData.replace(re1, '');
-                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a id="' + list[i].artiId + '">' +
+                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
                             list[0].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
                         $('.main .content').html(inform);
+                        count++;
 
                         for (var i = 1; i < articleLen; i++) {
                             var article = list[i].artiTitle.artiData.replace(re1, '');
-                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3>' +
+                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
                                 list[i].artiTitle + '</h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
                             $('.main .content').append(inform);
+                            count++;
                         }
                     }
                     else {
@@ -109,17 +113,20 @@ $(document).ready(function () {
                         var list = data.jsonStrArray.artiList;
                         var articleLen = list.length;
                         var re1 = new RegExp("<.+?>", "g");
+                        var count = 0;
 
                         var article = list[0].artiTitle.artiData.replace(re1, '');
-                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a id="' + list[i].artiId + '">' +
+                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
                             list[0].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
                         $('.main .content').html(inform);
+                        count++;
 
                         for (var i = 1; i < articleLen; i++) {
                             var article = list[i].artiTitle.artiData.replace(re1, '');
-                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a>' +
+                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
                                 list[i].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
                             $('.main .content').append(inform);
+                            count++;
                         }
                     }
                     else {
@@ -134,9 +141,14 @@ $(document).ready(function () {
     });
 
     // 新闻详情页面跳转
-    $('.col-sm-6 .caption h3 a').on('click',function () {
-        var articleId = $(this).attr('id');
-        var url = 'article.html?artiId=' +  articleId;
+    $('.col-sm-6 .caption h3 a').on('click', function () {
+        var id = $(this).attr('id');
+        var number = $(this).attr('class');
+
+        $.cookie('artiId', id, {path:"/"});
+        $.cookie('pageNum', number, {path:"/"});
+
+        var url = 'article.html?artiId=' + id;
         window.open(url);
     })
 });
