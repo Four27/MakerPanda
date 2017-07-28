2fb19740611e776eb2c525bc251ed2386ac509c1
Shirley <1733025299@qq.com>
2017-07-28 17:54:31 +0800

    add masonry layouts

--- js/articleList.js
+++ js/articleList.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
                 if (data.status === 200) {
                     var list = data.jsonStrArray.artiList;
                     var articleLen = list.length;
-                    var re1 = new RegExp("<.+?>", "g");
+                    var re1 = /<.+?>/g;
                     var pageNum = parseInt(data.jsonStrArray.artiNum / 15);
                     var pagemode = data.jsonStrArray.artiNum % 15;
                     var count = 0;
@@ -28,12 +28,18 @@ $(document).ready(function () {
                     pageNumber = pageNum;
 
                     for (var i = 0; i < articleLen; i++) {
-                        var article = list[i].artiTitle.artiData.replace(re1, '');
-                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="1" id="' + count + '">'
-                        list[i].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>'
-                        $('.main .content').append(inform);
+                        var article = list[i].artiData.replace(re1, '');
+                        var inform = $('<div class="col-sm-6 col-md-4 item"><div class="thumbnail"><div class="caption"><h3><a class="1" id="' + count + '">' +
+                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 150) + '...</p></div></div></div>');
+
+                        inform.appendTo('.main .content');
                         count++;
                     }
+
+                    $('.main .content').masonry({
+                        itemSelector: '.item'
+                    });
+
                 }
                 else {
                     alert(data.errMsg + data.status);
@@ -46,102 +52,110 @@ $(document).ready(function () {
     });
 
     //下一页
-    $('.next').click(function () {
-        $(function getArticle(page) {
-            page++;
+    function nextArticle() {
+        if (page === pageNumber) {
+            alert('已经到文章最后一页~');
+            return true;
+        }
 
-            if (page > pageNum) {
-                alert('已经到文章最后一页~');
-                return true;
-            }
+        page++;
 
-            $.ajax({
-                type: 'post',
-                url: ' /ShowBriefArticle',
-                dataType: 'json',
-                data: {
-                    pageNum: page
-                },
-                success: function (data) {
-                    if (data.status === 200) {
-                        var list = data.jsonStrArray.artiList;
-                        var articleLen = list.length;
-                        var re1 = new RegExp("<.+?>", "g");
-                        var count = 0
+        $.ajax({
+            type: 'post',
+            url: ' /ShowBriefArticle',
+            dataType: 'json',
+            data: {
+                pageNum: page
+            },
+            success: function (data) {
+                if (data.status === 200) {
+                    var list = data.jsonStrArray.artiList;
+                    var articleLen = list.length;
+                    var re1 = new RegExp("<.+?>", "g");
+                    var count = 0
 
-                        var article = list[0].artiTitle.artiData.replace(re1, '');
-                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
-                            list[0].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
-                        $('.main .content').html(inform);
+                    $('.main .content').empty();
+
+                    for (var i = 0; i < articleLen; i++) {
+                        var article = list[i].artiData.replace(re1, '');
+                        var inform = $('<div class="col-sm-6 col-md-4 item"><div class="thumbnail"><div class="caption"><h3><a class="1" id="' + count + '">' +
+                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 150) + '...</p></div></div></div>');
+
+                        inform.appendTo('.main .content');
                         count++;
+                    }
 
-                        for (var i = 1; i < articleLen; i++) {
-                            var article = list[i].artiTitle.artiData.replace(re1, '');
-                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
-                                list[i].artiTitle + '</h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
-                            $('.main .content').append(inform);
-                            count++;
-                        }
-                    }
-                    else {
-                        alert(data.errMsg + data.status);
-                    }
-                },
-                error: function (jqXHR) {
-                    alert('未知错误：' + jqXHR.status);
+                    $('.main .content').masonry({
+                        itemSelector: '.item'
+                    });
+
                 }
-            });
+                else {
+                    alert(data.errMsg + data.status);
+                }
+            },
+            error: function (jqXHR) {
+                alert('未知错误：' + jqXHR.status);
+            }
         });
-    });
+    }
 
     //上一页
-    $('.previous').click(function () {
-        $(function getArticle(page) {
-            page--;
+    function preArticle() {
+        if (page === 1) {
+            alert('已经到文章第一页~');
+            return true;
+        }
 
-            if (page === 0) {
-                alert('已经到文章第一页~');
-                return true;
-            }
+        page--;
 
-            $.ajax({
-                type: 'post',
-                url: ' /ShowBriefArticle',
-                dataType: 'json',
-                data: {
-                    pageNum: page
-                },
-                success: function (data) {
-                    if (data.status === 200) {
-                        var list = data.jsonStrArray.artiList;
-                        var articleLen = list.length;
-                        var re1 = new RegExp("<.+?>", "g");
-                        var count = 0;
+        $.ajax({
+            type: 'post',
+            url: ' /ShowBriefArticle',
+            dataType: 'json',
+            data: {
+                pageNum: page
+            },
+            success: function (data) {
+                if (data.status === 200) {
+                    var list = data.jsonStrArray.artiList;
+                    var articleLen = list.length;
+                    var re1 = new RegExp("<.+?>", "g");
+                    var count = 0;
 
-                        var article = list[0].artiTitle.artiData.replace(re1, '');
-                        var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
-                            list[0].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
-                        $('.main .content').html(inform);
+                    $('.main .content').empty();
+
+                    for (var i = 0; i < articleLen; i++) {
+                        var article = list[i].artiData.replace(re1, '');
+                        var inform = '<div class="col-sm-6 col-md-4 item"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
+                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 150) + '...</p></div></div></div>';
+                        $('.main .content').append(inform);
                         count++;
+                    }
 
-                        for (var i = 1; i < articleLen; i++) {
-                            var article = list[i].artiTitle.artiData.replace(re1, '');
-                            var inform = '<div class="col-sm-6 col-md-4"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
-                                list[i].artiTitle + '</a></h3><p>' + article.substr(0, 50) + '</p></div></div></div>';
-                            $('.main .content').append(inform);
-                            count++;
-                        }
-                    }
-                    else {
-                        alert(data.errMsg + data.status);
-                    }
-                },
-                error: function (jqXHR) {
-                    alert('未知错误：' + jqXHR.status);
+                    $('.main .content').masonry({
+                        itemSelector: '.item'
+                    });
+
                 }
-            });
+                else {
+                    alert(data.errMsg + data.status);
+                }
+            },
+            error: function (jqXHR) {
+                alert('未知错误：' + jqXHR.status);
+            }
         });
+    }
+
+    $('.next').click(function () {
+        nextArticle();
     });
+
+    $('.previous').click(function () {
+        preArticle(page);
+    });
+
 
     // 新闻详情页面跳转
     $('.col-sm-6 .caption h3 a').on('click', function () {
