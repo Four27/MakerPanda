aa2715b3e4c9e1629d974d3631b258f340599eab
Shirley <1733025299@qq.com>
2017-07-29 20:59:03 +0800

    change style

--- js/articleList.js
+++ js/articleList.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var pageNumber = 0;
 
     //默认第一页
-    $(function getArticle() {
+    $(function getArticle(page) {
         $.ajax({
             type: 'post',
             url: ' /ShowBriefArticle',
@@ -27,10 +27,12 @@ $(document).ready(function () {
 
                     pageNumber = pageNum;
 
+                    $('.main .content').empty();
+
                     for (var i = 0; i < articleLen; i++) {
                         var article = list[i].artiData.replace(re1, '');
                         var inform = $('<div class="col-sm-6 col-md-4 item"><div class="thumbnail"><div class="caption"><h3><a class="1" id="' + count + '">' +
-                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 150) + '...</p></div></div></div>');
+                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 110) + '...</p></div></div></div>');
 
                         inform.appendTo('.main .content');
                         count++;
@@ -79,15 +81,18 @@ $(document).ready(function () {
                     for (var i = 0; i < articleLen; i++) {
                         var article = list[i].artiData.replace(re1, '');
                         var inform = $('<div class="col-sm-6 col-md-4 item"><div class="thumbnail"><div class="caption"><h3><a class="1" id="' + count + '">' +
-                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 150) + '...</p></div></div></div>');
+                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 110) + '...</p></div></div></div>');
 
                         inform.appendTo('.main .content');
                         count++;
                     }
 
-                    $('.main .content').masonry({
-                        itemSelector: '.item'
+                    var $grid = $('.main .content').masonry({
+                        itemSelector: '.item',
+                        percentPosition: true
                     });
+
+                    $('.main .content').height('100%');
 
                 }
                 else {
@@ -121,21 +126,24 @@ $(document).ready(function () {
                     var list = data.jsonStrArray.artiList;
                     var articleLen = list.length;
                     var re1 = new RegExp("<.+?>", "g");
-                    var count = 0;
+                    var count = 0
 
                     $('.main .content').empty();
 
                     for (var i = 0; i < articleLen; i++) {
                         var article = list[i].artiData.replace(re1, '');
-                        var inform = '<div class="col-sm-6 col-md-4 item"><div class="thumbnail"><div class="caption"><h3><a class="' + page + '" id="' + count + '">' +
-                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 150) + '...</p></div></div></div>';
-                        $('.main .content').append(inform);
+                        var inform = $('<div class="col-sm-6 col-md-4 item"><div class="thumbnail"><div class="caption"><h3><a class="1" id="' + count + '">' +
+                            list[i].artiTitle + '</a></h3><p>' + article.substr(0, 110) + '...</p></div></div></div>');
+
+                        inform.appendTo('.main .content');
                         count++;
                     }
 
                     $('.main .content').masonry({
                         itemSelector: '.item'
                     });
+
+                    $('.main .content').height('100%');
 
                 }
                 else {
@@ -158,14 +166,14 @@ $(document).ready(function () {
 
 
     // 新闻详情页面跳转
-    $('.col-sm-6 .caption h3 a').on('click', function () {
+    $(document).on('click', '.col-sm-6 h3 a', function () {
         var id = $(this).attr('id');
         var number = $(this).attr('class');
 
         $.cookie('artiId', id, { path: "/" });
         $.cookie('pageNum', number, { path: "/" });
 
-        var url = 'article.html?artiId=' + id;
+        var url = 'article.html';
         window.open(url);
     })
 });
